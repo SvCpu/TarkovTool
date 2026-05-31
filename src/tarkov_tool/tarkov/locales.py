3863bc53c739ebd577e51761541469dfdfdd9ce3
SvCpu <2582439@gmail.com>
2026-05-31 10:38:05 +0800

    move locale file

--- src/tarkov_tool/tarkov/locales.py
+++ src/tarkov_tool/tarkov/locales.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import importlib.resources as resources
 
-locales_dir = resources.files("tarkov_tool.localization")
+locales_dir = resources.files("tarkov_tool.tarkov.localization")
 
 print(list(locales_dir.iterdir()))
