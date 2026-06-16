67e7f8a30f4c3dd92251df3ffec739dd57a51856
SvCpu <2582439@gmail.com>
2026-06-17 03:56:17 +0800

    feat:  - add method to generate tarkov format screenshot file name  - get_screenshot_info method now will check filename suffix

--- src/tarkov_tool/tarkov/screenshot.py
+++ src/tarkov_tool/tarkov/screenshot.py
@@ -1,13 +1,37 @@
 from .datamodel import ScreenshotInfo, Position
 
+from datetime import datetime
 import re
 import math
 from pathlib import Path
+from typing import Iterable
 
-def get_screenshot_info_from_filename(filename:Path)->ScreenshotInfo|None:
-    if not isinstance(filename,Path):
+__all__ = [
+    'get_screenshot_info',
+    'generate_screenshot_filename',
+    'generate_unique_screenshot_filename',
+]
+
+def generate_screenshot_filename(index: int = 0)->str:
+    now = datetime.now()
+    filename = now.strftime(f"%Y-%m-%d[%H-%M] ({index}).png")
+    return filename
+
+def generate_unique_screenshot_filename(existing: Iterable[str]) -> str:
+    existing = set(existing)
+    index = 0
+    while True:
+        filename = generate_screenshot_filename(index)
+        if filename not in existing:
+            return filename
+        index += 1
+
+def get_screenshot_info(filepath:Path)->ScreenshotInfo|None:
+    if not isinstance(filepath,Path):
         raise TypeError
-    filename:str = filename.name
+    if filepath.suffixes != '.png':
+        return None
+    info = ScreenshotInfo(filepath=filepath, filename=filepath.name)
     def quaternions_to_yaw(rx: float, ry: float, rz: float, rw: float) -> float:
         siny_cosp = 2 * (rw * rz + rx * ry)
         cosy_cosp = 1 - 2 * (ry * ry + rz * rz)
@@ -26,21 +50,20 @@ def get_screenshot_info_from_filename(filename:Path)->ScreenshotInfo|None:
         r"(?P<x>-?\d+\.\d+), (?P<y>-?\d+\.\d+), (?P<z>-?\d+\.\d+)_"
         r"(?P<rx>-?\d+\.\d+), (?P<ry>-?\d+\.\d+), (?P<rz>-?\d+\.\d+), (?P<rw>-?\d+\.\d+)_"
         r"(?P<angle>-?\d+\.\d+) \(\d\)\.png$",
-        filename
+        filepath.name
     )
-    if not match:
-        return None
+    if match:
 
-    x = float(match.group("x"))
-    y = float(match.group("y"))
-    z = float(match.group("z"))
-    rx = float(match.group("rx"))
-    ry = float(match.group("ry"))
-    rz = float(match.group("rz"))
-    rw = float(match.group("rw"))
+        x = float(match.group("x"))
+        y = float(match.group("y"))
+        z = float(match.group("z"))
+        rx = float(match.group("rx"))
+        ry = float(match.group("ry"))
+        rz = float(match.group("rz"))
+        rw = float(match.group("rw"))
 
-    yaw = quaternions_to_yaw(rx, ry, rz, rw)
-    pitch = quaternions_to_pitch(rx, ry, rz, rw)
-    pos = Position(x, y, z)
+        info.yaw = quaternions_to_yaw(rx, ry, rz, rw)
+        info.pitch = quaternions_to_pitch(rx, ry, rz, rw)
+        info.position = Position(x=x, y=y, z=z)
 
-    return ScreenshotInfo(filename=filename, position=pos, yaw=yaw, pitch=pitch)
+    return info
