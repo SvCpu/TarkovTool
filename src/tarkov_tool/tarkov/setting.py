7f6b10108ac48f7911c0c684a946287c39e76f45
SvCpu <2582439@gmail.com>
2026-06-23 18:25:38 +0800

    feat(tarkov.setting):  - 添加部分欄位的描述  - 添加部分欄位的數據驗證

--- src/tarkov_tool/tarkov/setting.py
+++ src/tarkov_tool/tarkov/setting.py
@@ -1,8 +1,9 @@
-from typing import List, Optional, Literal
-from pydantic import BaseModel
+from typing import Annotated, List, Optional, Literal
+
+from pydantic import BaseModel, Field, model_validator
 
 class GameSettings(BaseModel):
-    Language: str
+    Language: Literal['ch','cz','en','fr','ge','hu','it','jp','kr','pl','po','sk','es','es-mx','tu','ru','ro','vi','in','th']
     SelectedMemberCategory: str
     QuickSlotsVisibility: str
     StaminaVisibility: str
@@ -22,8 +23,8 @@ class GameSettings(BaseModel):
     PriorityWindowMode: str
     AutoVaultingMode: str
     ContinuousHealMode: str
-    QuestItemNotificationMode: str
-    QuestItemSearchMode: str
+    QuestItemNotificationMode: Optional[str] = Field(default=None)
+    QuestItemSearchMode: Optional[str] = Field(default=None)
     WishlistNotificationsType: str
     AutoAddToWishlist: str
     MalfunctionVisability: bool
@@ -41,22 +42,22 @@ class SoundSettings(BaseModel):
     MusicOnRaidEnd: bool
     VoipEnabled: bool
     VoipDevice: str
-    VoiceChatVolume: int
-    MicrophoneSensitivity: int
-    DenoiseAmount: str
+    VoiceChatVolume: Optional[int] = Field(le=100, ge=0, default=None)
+    MicrophoneSensitivity: Optional[int] = Field(default=None)
+    DenoiseAmount: Optional[str] = Field(default=None)
 
 class PostFxSettings(BaseModel):
     EnablePostFx: bool
-    Brightness: int
-    Saturation: int
-    Clarity: int
-    Colorfulness: int
-    LumaSharpen: int
-    AdaptiveSharpen: int
-    ColorFilterType: str
-    Intensity: int
-    ColorBlindnessType: str
-    ColorBlindnessIntensity: int
+    Brightness: int = Field(description='亮度', ge=-100, le=100)
+    Saturation: int = Field(description='Unknow')
+    Colorfulness: int = Field(description='Unknow')
+    Clarity: int = Field(description='清晰度', ge=-100, le=100)
+    LumaSharpen: int = Field(description='光線銳化', ge=0, le=100)
+    AdaptiveSharpen: int = Field(description='適應性銳化', ge=0, le=100)
+    ColorFilterType: Literal['None', 'K506', 'Zabid', 'Cognac', 'Edwards', 'Cheese', 'LateGoose', 'Bread', 'Montreal', 'Feather', 'Jason', 'Fahrenheit', 'Owl', 'Chillwave', 'Albert', 'Bayswater', 'Atlanta', 'Felicity', 'Stefano', 'Boost', 'Emilia', 'Doze', 'Clifden', 'Blender', 'Tokyo', 'Walk', 'Olive', 'Hotshot'] = Field(description='顏色分級')
+    Intensity: int = Field(description='顏色分級強度', ge=0, le=100)
+    ColorBlindnessType: Literal['None', 'Tritanopia','Protanopia','Deuteranopia'] = Field(description='色盲模式')# Tritanopia:藍色盲, Protanopia:紅色盲, Deuteranopia:綠色盲
+    ColorBlindnessIntensity: int = Field(description='色盲模式強度', ge=0, le=100)
 
 # Graphics settings
 class Resolution(BaseModel):
@@ -108,10 +109,10 @@ class GraphicsSettings(BaseModel):
     ChromaticAberrations: bool
     Noise: bool
     ZBlur: bool
-    AreaLightsInstancing: bool
+    AreaLightsInstancing: Optional[bool] = Field(default=None)
     HighQualityColor: bool
     SdTarkovStreets: bool
-    VolumetricLight: str
+    VolumetricLight: str|bool
     DLSSMode: str
     FSR2Mode: str
     FSR3Mode: str
@@ -122,6 +123,12 @@ class GraphicsSettings(BaseModel):
     FSR3Enabled: bool
     ShadowDistance: float
     SuperSamplingFactor: float
+    MipStreaming: Optional[bool] = Field(default=None, deprecated=True)
+    @model_validator(mode='after')
+    def check_Upscaling_enabled(self)->'GraphicsSettings':
+        if sum([self.DLSSEnabled, self.FSR2Enabled, self.FSR3Enabled]) >1:
+            pass
+        return self
 
 # Control settings
 class KeyCodeBinding(BaseModel):
@@ -147,15 +154,16 @@ class Variant(BaseModel):
 
 class KeyBinding(BaseModel):
     keyName: str
-    variants: List[Variant]
+    variants: Annotated[list[Variant], Field(min_length=2, max_length=2)]
     pressType: Literal['DoubleClick', 'Continuous', 'Press', 'Release']
+    # Press:按下;DoubleClick:雙擊;Release:鬆開;Continuous:按住
 
 class ControlSettings(BaseModel):
-    InvertedXAxis: bool
-    InvertedYAxis: bool
+    InvertedXAxis: bool = Field(description='反轉 x 軸')
+    InvertedYAxis: bool = Field(description='反轉 y 軸')
     MouseSensitivity: float
     MouseAimingSensitivity: float
-    DoubleClickTimeout: float
+    DoubleClickTimeout: float = Field(description='雙擊判斷間隔')
     OpticSensitivity: float
     axisBindings: List[AxisBinding]
     keyBindings: List[KeyBinding]
